5a840ae69496eb73382a724f7e944352ff7d7cdf
Krish Vijay Ramani <136609165+KrishRamani2@users.noreply.github.com>
2023-08-04 20:19:02 +0530

    Add files via upload

---
+++ 36.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class gfg 
+{
+public:
+	unsigned int countSetBits(int n)
+	{
+		unsigned int count = 0;
+		while (n) {
+			n &= (n - 1);
+			count++;
+		}
+		return count;
+	}
+};
+int main()
+{
+	gfg g;
+	int i = 9;
+	cout << g.countSetBits(i);
+	return 0;
+}
---
+++ 37.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int BitsSetTable256[256];
+void initialize()
+{
+	BitsSetTable256[0] = 0;
+	for (int i = 0; i < 256; i++)
+	{
+		BitsSetTable256[i] = (i & 1) +
+		BitsSetTable256[i / 2];
+	}
+}
+int countSetBits(int n)
+{
+	return (BitsSetTable256[n & 0xff] +
+			BitsSetTable256[(n >> 8) & 0xff] +
+			BitsSetTable256[(n >> 16) & 0xff] +
+			BitsSetTable256[n >> 24]);
+}
+int main()
+{
+	initialize();
+	int n = 9;
+	cout << countSetBits(n);
+}
